b0a0b61530d44aa246b4c8b75fe3e1fa10f9e881
LazaroPol <pol.lazaro.rosas@gmail.com>
2024-11-11 19:26:37 +0100

    create msg

--- src/main/java/com/pablomonteserin/login/persistence/services/MessageService.java
+++ src/main/java/com/pablomonteserin/login/persistence/services/MessageService.java
@@ -2,6 +2,7 @@ package com.pablomonteserin.login.persistence.services;
 
 import com.pablomonteserin.login.persistence.entities.Message;
 import com.pablomonteserin.login.persistence.entities.MessageDTO;
+import com.pablomonteserin.login.persistence.entities.User;
 import com.pablomonteserin.login.persistence.entities.UserDTO;
 import com.pablomonteserin.login.persistence.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,28 @@ public class MessageService {
     public List<MessageDTO> getMessagesByRemitenteId(Integer remitenteId) {
         List<Message> messages = messageRepository.findMessagesByRemitenteId(remitenteId);
         return messages.stream().map(this::toMessageDTO).collect(Collectors.toList());
+    }
+    public void createMessage(MessageDTO messageDTO) {
+        // Convierte MessageDTO a Message
+        Message message = toMessageEntity(messageDTO);
+        // Guarda la entidad Message en la base de datos
+        messageRepository.save(message);
+    }
+
+    private Message toMessageEntity(MessageDTO messageDTO) {
+        Message message = new Message();
+        message.setText(messageDTO.getText());
+
+        // Aquí asumimos que remitente y destinatario son IDs en el DTO
+        User user = new User();
+        user.setId(messageDTO.getRemitenteId());
+        message.setRemitente(user);
+
+        User destinatario = new User();
+        destinatario.setId(messageDTO.getDestinatarioId());
+        message.setDestinatarios(destinatario);
+
+        return message;
     }
 
 }
